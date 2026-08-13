7394725834b4c511b6ec8bdda3e386a684671259
Harshit Maurya <harsitmaurya555@gmail.com>
2026-08-13 20:52:48 +0530

    881

---
+++ ARRAYS/881. Boats to Save People.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+
+        int left = 0;
+        int right = people.size() - 1;
+        int boat = 0;
+        int rem = 0;
+
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                boat++;
+                left++;
+                right--;
+            }
+            else {
+                rem++;
+                right--;
+            }
+        }
+
+        return boat + rem;
+    }
+};
